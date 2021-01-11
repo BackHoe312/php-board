d9e0bf78b009aeb5486a754d96234fc46f5bdb30
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2021-01-11 12:49:34 +0900

    Create login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div align=center>
+        <span>로그인</span>
+
+        <form method='get' action='login+_action.php'>
+            <p>ID:<input name="id" type="text"></p>
+            <p>PW:<input name="pw" type="text"></p>
+            <input type="submit" value="로그인">
+        </form>
+        <br>
+        <button id="join" onclic="loaction.href='./join.php'">회원가입</button>
+    </div>
+</body>
+</html>
